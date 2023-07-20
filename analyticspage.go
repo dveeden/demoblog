27fab4c2ceba00ad57756839e8b23fc39568e1ac
Daniël van Eeden <git@myname.nl>
2023-07-20 10:23:28 +0200

    Add first stats/analytics

--- analyticspage.go
+++ analyticspage.go
@@ -13,6 +13,39 @@ import (
 //go:embed html/analytics.html
 var analyticsTmpl string
 
+type analytics struct {
+	db              *sql.DB
+	CommentsPerPost map[string]uint64
+}
+
+func (a *analytics) commentsPerPost() {
+	var (
+		title string
+		count uint64
+	)
+	q :=
+		`SELECT
+			p.title,
+			COUNT(c.id)
+		FROM
+			posts p
+		LEFT JOIN
+			comments c ON p.id=c.post_id
+		GROUP BY
+			p.id`
+
+	a.CommentsPerPost = make(map[string]uint64, 10)
+	rows, err := a.db.Query(q)
+	for rows.Next() {
+		if err != nil {
+			log.Print(err)
+		} else {
+			rows.Scan(&title, &count)
+			a.CommentsPerPost[title] = count
+		}
+	}
+}
+
 func analyticsPage(w http.ResponseWriter, r *http.Request) {
 	var t = template.Must(template.New("analytics").Parse(analyticsTmpl))
 
@@ -25,7 +58,10 @@ func analyticsPage(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	err = t.Execute(w, nil)
+	a := analytics{db: db}
+	a.commentsPerPost()
+
+	err = t.Execute(w, a)
 	if err != nil {
 		log.Print(err)
 	}
