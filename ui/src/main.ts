c84aab4f9c099529a84ea66c8b93a9a43a520a56
Daniël van Eeden <git@myname.nl>
2023-08-07 22:51:40 +0200

    Add fontawesome and a like button

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,7 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faNewspaper } from '@fortawesome/free-regular-svg-icons'
+library.add(faNewspaper)
+
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons'
+library.add(faThumbsUp)
+
 const app = createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router)
 
--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -17,15 +17,19 @@ function postFetch() {
 
 }
 const posts: Ref<Post[]>  = postFetch()
+
+function like(postId: number) {
+	console.log("Liking " + postId);
+}
 </script>
 
 <template>
 	<template v-for="post in posts" :key="post.Id">
-		<h1>{{ post.Title }}</h1><br>
+		<h1><font-awesome-icon :icon="['far', 'newspaper']" /> {{ post.Title }}</h1><br>
 		{{ post.Body.substring(0,100) }}
 		<br>
+		<button @click="like(post.Id)"><font-awesome-icon :icon="['far', 'thumbs-up']" /> Like</button>&nbsp;
 		<router-link :to="{ name: 'post', params: { id: post.Id }}">Read more...</router-link>
 	<br>
 	</template>
 </template>
-../components/global
